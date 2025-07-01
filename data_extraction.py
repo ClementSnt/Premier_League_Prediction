9d4cf5b386ce8d721830df5be9edde4bb93916bd
ClementSnt <clement.sonnet@protonmail.com>
2025-07-01 15:02:18 +0200

    Add files via upload

---
+++ data_extraction.py
@@ -0,0 +1,30 @@
+import requests
+import pandas as pd
+from pandas import json_normalize
+
+API_KEY = YOUR_API_KEY # mettre votre API KEY
+
+
+LEAGUE_ID = 1625  # selection de la ligue souhaitée, ici Premier League saison 2018-2019
+
+url = f"https://api.footystats.org/league-matches?key={API_KEY}&league_id={LEAGUE_ID}"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    json_data = response.json()
+    data = json_data.get("data", [])
+
+    if data:
+        # Aplatir les structures imbriquées
+        df = json_normalize(data)
+
+        # Sauvegarde du df dans un .csv
+        df.to_csv("matches_premier_league2019.csv", index=False)
+
+        print("Import données ok")
+        print(df.head())  # Check de l'import
+    else:
+        print("⚠️ Aucune donnée reçue.")
+else:
+    print(f"Erreur!! {response.status_code} : {response.text}")
